541837a1f48a84ef531642cb655e78398e6b8502
N29Martz <franmlopez29@gmail.com>
2023-01-30 19:48:34 -0600

    Scanner y Anatomia

---
+++ basico/src/tipos/Leer.java
@@ -0,0 +1,21 @@
+package tipos;
+
+import java.util.Scanner;
+
+public class Leer{
+    public static void main(String[] args){
+        //Declarar variables
+        //Tipo de dato
+        //Indicar el nombre de la variable
+        //(opcional) asignar un valor a la variable
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.println("------------------------");
+        System.out.println("Hola " + nombre);
+        
+    }
+}
